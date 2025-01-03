82d58a43186a404e3864abedda6090a9577d53e8
giorgio <giorgio.facu.4@gmail.com>
2025-01-03 18:36:51 -0500

    navbar cambios

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="flex space-x-4">
     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-    <nav-link href="/" :active="request()->is('/')">Inicio</nav-link>
-    <nav-link href="/productos" :active="request()->is('/productos')">Productos</nav-link>
-    <nav-link href="/ubicacion" :active="request()->is('/ubicacion')" >Ubicacion</nav-link>
-    <nav-link href="/nosotros" :active="request()->is('/nosotros')" >Calendar</nav-link>
+    <x-nav-link href="/" :active="request()->is('/')">Inicio</x-nav-link>
+    <x-nav-link href="/productos" :active="request()->is('/productos')">Productos</x-nav-link>
+    <x-nav-link href="/ubicacion" :active="request()->is('/ubicacion')" >Ubicacion</x-nav-link>
+    <x-nav-link href="/nosotros" :active="request()->is('/nosotros')" >Calendar</x-nav-link>
   </nav>
